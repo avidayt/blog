60ea3e76f9a4e21678da8d3728b36e7454f5e0d9
davidyates <99277896+avidayt@users.noreply.github.com>
2022-02-08 18:16:24 +0000

    Trick replit

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $REPL_SLUG == 'CoarseDeafeningDisks' ] # draft space
+if [ "$REPL_SLUG" == 'CoarseDeafeningDisks' ] # draft space
 then 
   hugo serve --buildDrafts --buildFuture --bind 0.0.0.0 --port 443 --baseURL https://$REPL_SLUG.$REPL_OWNER.repl.co
 else # production
